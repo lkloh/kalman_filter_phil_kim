804a62c8eaf5d61c0c2b4ff19ad569da63e0bcf7
LayKuan Loh <lkloh2410@gmail.com>
2021-07-21 08:13:48 -0700

    low-pass filter fake data

---
+++ recursive_filter/1st_order_low_pass_filter/low_pass_filter.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+'''
+Chapter 3.2: 1st order low-pass filter
+
+
+'''
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+
+NUM_SAMPLES = 100
+ALPHA = 0.5 # 0 < ALPHA < 1
+DELTA_TIME = 1
+
+
+def low_pass_filter(estimate, data_point):
+    return ALPHA * estimate + (1 - ALPHA) * data_point
+
+
+def generate_random_data_point(approximate):
+    temp = approximate + 4 * random.triangular(-1, 1, 0)
+    return temp * temp
+
+def plot_data(timestamps,  measurements, estimates):
+    plt.title("Upwards trend data against time")
+    plt.xlabel("Time]")
+    plt.ylabel("Upwards trend data")
+    plt.plot(timestamps, measurements, label="Measurement", marker=".")
+    plt.plot(timestamps, estimates, label="Low-pass estimate")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    prev_estimate = None
+
+    timestamps = np.zeros(NUM_SAMPLES)
+    measurements = np.zeros(NUM_SAMPLES)
+    estimates = np.zeros(NUM_SAMPLES)
+
+    for sample_count in range(NUM_SAMPLES):
+        data_point = generate_random_data_point(sample_count)
+
+        if prev_estimate is None:
+            prev_estimate = data_point
+        curr_estimate = low_pass_filter(prev_estimate, data_point)
+        print("Low-pass estimate with alpha=%f at sample count %i for data_point %f is %f" % (ALPHA, sample_count, data_point, curr_estimate))
+
+        timestamps[sample_count] = DELTA_TIME * sample_count
+        measurements[sample_count] = data_point
+        estimates[sample_count] = curr_estimate
+
+        prev_estimate = curr_estimate
+
+    plot_data(timestamps, measurements, estimates)
+
