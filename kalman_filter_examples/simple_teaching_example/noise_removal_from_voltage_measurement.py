64ca732a5754cdacfee71262c0dff1e7b41443c0
LayKuan Loh <lkloh2410@gmail.com>
2021-07-24 10:20:39 -0700

    1D array implementation

--- kalman_filter_examples/simple_teaching_example/noise_removal_from_voltage_measurement.py
+++ kalman_filter_examples/simple_teaching_example/noise_removal_from_voltage_measurement.py
@@ -5,6 +5,8 @@ Chapter 10: Extremely Simple Example
 """
 from matplotlib import pyplot as plt
 import numpy as np
+
+# from numpy.linalg import inv
 import random
 
 NUM_SAMPLES = 100
@@ -12,10 +14,10 @@ DELTA_TIME = 0.2
 BATTERY_VOLTAGE = 14
 
 # System model parameters for Kalman Filter
-A = 1  # state transition
-H = 1  # state -> measurement
-Q = 0  # Covariance of state transition noise
-R = 2 * 2  # Covariance of measurement noise
+A = np.array([1])  # state transition
+H = np.array([1])  # state -> measurement
+Q = np.array([0])  # Covariance of state transition noise
+R = np.array([2 * 2])  # Covariance of measurement noise
 
 
 def generate_random_voltage_measurement():
@@ -35,18 +37,23 @@ def plot_data(timestamps, measurements, estimates):
     plt.show()
 
 
-def simple_kalman_filter(prev_x_estimate, prev_P_estimate):
-    A_T = A
-    A_inv = A
-    H_T = H
+def inverse_of_1D_matrix(mat):
+    # np.linalg.inv does not handle 1D matrix inverse
+    return np.array([1 / mat[0]])
 
+
+def simple_kalman_filter(prev_x_estimate, prev_P_estimate):
     # Step I: Predict state
     x_predict = A * prev_x_estimate
     # Step I: Predict error covariance
-    P_predict = A * prev_P_estimate * A_T + Q
+    P_predict = A * prev_P_estimate * A.transpose() + Q
 
     # Step II: Compute Kalman Gain
-    K = P_predict * H_T * (1 / (H * P_predict * H_T + R))
+    K = (
+        P_predict
+        * H.transpose()
+        * inverse_of_1D_matrix(H * P_predict * H.transpose() + R)
+    )
 
     # Step III: Compute estimate
     x_estimate = x_predict + K * (z - H * x_predict)
@@ -62,10 +69,10 @@ if __name__ == "__main__":
     saved_measurements = np.zeros(NUM_SAMPLES)
     saved_estimates = np.zeros(NUM_SAMPLES)
 
-    x_estimate = BATTERY_VOLTAGE  # Initial state estimate
-    P_estimate = (
-        6  # Make estimated error covariance on the large size for initial condition
-    )
+    x_estimate = np.array([BATTERY_VOLTAGE])  # Initial state estimate
+    P_estimate = np.array(
+        [6]
+    )  # Make estimated error covariance on the larger size for initial condition
 
     for i in range(NUM_SAMPLES):
         z = generate_random_voltage_measurement()
@@ -75,6 +82,6 @@ if __name__ == "__main__":
         # Save results for plotting
         saved_timestamps[i] = DELTA_TIME * i
         saved_measurements[i] = z
-        saved_estimates[i] = x_estimate
+        saved_estimates[i] = x_estimate[0]
 
     plot_data(saved_timestamps, saved_measurements, saved_estimates)
