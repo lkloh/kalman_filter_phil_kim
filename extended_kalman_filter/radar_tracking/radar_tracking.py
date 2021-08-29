dd787c24acf8becb5b72fe4f19db121eb5407e0c
LayKuan Loh <lkloh2410@gmail.com>
2021-08-29 15:59:21 -0700

    init radar folder

---
+++ extended_kalman_filter/radar_tracking/radar_tracking.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+from plotly.subplots import make_subplots
+import plotly.graph_objects as plotly_go
+from scipy.io import loadmat
+
+if __name__ == "__main__":
+    print('helo')
