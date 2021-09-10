17cf5a84db42afd44cb9d3d909c2d3058a4aafd3
LayKuan Loh <lkloh2410@gmail.com>
2021-09-10 08:36:59 -0700

    ut

--- unscented_kalman_filter/unscented_transformation.py
+++ unscented_kalman_filter/unscented_transformation.py
@@ -13,18 +13,29 @@ kappa: arbitrary constant
 
 def compute_sigma_points(xm, P, kappa):
     n_vars = len(xm)
-    sigma_points = np.zeros(shape=(n_vars, 2 * n_vars + 1))
-    weights = np.zeros(shape=(n_vars, 1))
+    sigma_points = np.zeros(shape=(2 * n_vars + 1, n_vars))
+    weights = np.zeros(shape=(2 * n_vars + 1, 1))
 
     for i in range(n_vars):
         sigma_points[i, 0] = xm[i]
     weights[0] = kappa / (n_vars + kappa)
 
+    # Where U^T @ U = (n_vars + kappa) * P
+    U = np.linalg.cholesky((n_vars + kappa) * P)
+
+    for i in range(n_vars):
+        sigma_points[i + 1, :] = xm + U[i, :]
+        weights[i + 1] = 1 / (2 * (n_vars + kappa))
+    for i in range(n_vars):
+        sigma_points[n_vars + i + 1, :] = xm - U[i, :]
+        weights[n_vars + i + 1] = 1 / (2 * (n_vars + kappa))
+
     print(sigma_points)
     print(weights)
+    print(U)
 
 
 if __name__ == "__main__":
-    xm = np.array([[1], [2], [3]])
-    P = np.array([[1.3], [1.2], [0.9]])
+    xm = np.array([1, 2, 3])
+    P = 1.2 * np.eye(3)
     compute_sigma_points(xm, P, 2)
