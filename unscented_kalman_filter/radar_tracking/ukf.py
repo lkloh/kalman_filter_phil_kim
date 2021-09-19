75332fbb68eac95857422706a5b2f1b57c075076
LayKuan Loh <lkloh2410@gmail.com>
2021-09-19 15:06:55 -0700

    fix dim

--- unscented_kalman_filter/radar_tracking/ukf.py
+++ unscented_kalman_filter/radar_tracking/ukf.py
@@ -3,7 +3,8 @@
 import numpy as np
 from numpy.linalg import inv
 import math
-from unscented_transformation import (compute_sigma_points, unscented_transformation)
+from unscented_transformation import compute_sigma_points, unscented_transformation
+
 
 def fx(state, dt):
     A = np.array(
@@ -23,6 +24,7 @@ def hx(state):
     slant_dist = math.sqrt(math.pow(horizontal_dist, 2) + math.pow(altitude, 2))
     return slant_dist
 
+
 class RadarUKF:
     def __init__(self, Q, R, kappa, dt, n_state_vars, n_measurements):
         self.Q = Q  # covariance of state transformation
@@ -35,4 +37,18 @@ class RadarUKF:
     def run_ukf(self, meas, prev_state, prev_covariance):
         # Compute Sigma points and weights
         [sigma, weights] = compute_sigma_points(prev_state, prev_covariance, self.kappa)
-        print(sigma)
+
+        # Predict state and error covariange
+        f_sigma = np.zeros(shape=(self.n_state_vars, 2*self.n_state_vars + 1))
+        for k in range(2 * self.n_state_vars + 1):
+            f_sigma[:,k] = fx(sigma[:,k], self.dt)
+        [predicted_state, predicted_state_transition_cov] = unscented_transformation(f_sigma, weights, self.Q)
+
+        # predict measurement and covariance
+        h_sigma = np.zeros(shape=(self.n_measurements, 2*self.n_state_vars + 1))
+        for k in range(2 * self.n_state_vars + 1):
+            h_sigma[:, k] = hx(f_sigma[:,k])
+        [predicted_meas, predicated_meas_cov] = unscented_transformation(h_sigma, weights, self.R)
+
+        # compute kalman gain
+
--- unscented_kalman_filter/radar_tracking/unscented_transformation.py
+++ unscented_kalman_filter/radar_tracking/unscented_transformation.py
@@ -7,38 +7,39 @@ P: covariance
 kappa: arbitrary constant
 """
 
+
 def compute_sigma_points(xm, P, kappa):
     n_vars = len(xm)
-    sigma_points = np.zeros(shape=(2 * n_vars + 1, n_vars))
+    sigma_points = np.zeros(shape=(n_vars, 2 * n_vars + 1))
     weights = np.zeros(2 * n_vars + 1)
 
     for i in range(n_vars):
-        sigma_points[i, 0] = xm[i]
+        sigma_points[:, i] = xm
     weights[0] = kappa / (n_vars + kappa)
 
     # Where U^T @ U = (n_vars + kappa) * P
     U = np.linalg.cholesky((n_vars + kappa) * P)
 
     for i in range(n_vars):
-        sigma_points[i + 1, :] = xm + U[i, :]
+        sigma_points[:, i + 1] = xm + U[:, i]
         weights[i + 1] = 1 / (2 * (n_vars + kappa))
     for i in range(n_vars):
-        sigma_points[n_vars + i + 1, :] = xm - U[i, :]
+        sigma_points[:, n_vars + i + 1] = xm - U[i, :]
         weights[n_vars + i + 1] = 1 / (2 * (n_vars + kappa))
 
     return [sigma_points, weights]
 
 
 def unscented_transformation(sigma_points, weights, noise_covariance):
-    [n_sigma_pts, n_state_vars] = sigma_points.shape
+    [n_state_vars, n_sigma_pts] = sigma_points.shape
 
     new_xm = np.zeros(n_state_vars)
     for i in range(n_sigma_pts):
-        new_xm += weights[i] * sigma_points[i, :]
+        new_xm += weights[i] * sigma_points[:, i]
 
     new_P = np.zeros(shape=(n_state_vars, n_state_vars))
     for i in range(n_sigma_pts):
-        temp = sigma_points[i, :] - new_xm
+        temp = sigma_points[:, i] - new_xm
         new_P += weights[i] * (temp @ temp.transpose())
 
     return [new_xm, new_P + noise_covariance]
