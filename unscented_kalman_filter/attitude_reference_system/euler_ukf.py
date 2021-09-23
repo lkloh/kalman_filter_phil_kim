c00cb15ebfcee560b270238c0a2912591ecfc074
LayKuan Loh <lkloh2410@gmail.com>
2021-09-23 08:00:02 -0700

    euler ukf

--- unscented_kalman_filter/attitude_reference_system/euler_ukf.py
+++ unscented_kalman_filter/attitude_reference_system/euler_ukf.py
@@ -4,9 +4,16 @@ import numpy as np
 from ut import compute_sigma_points, unscented_transformation
 
 
-def fx(estimated_state, angular_rates, dt):
-    phi = estimated_state[0]
-    theta = estimated_state[1]
+def hx(state):
+    phi = state[0]
+    theta = state[1]
+
+    return np.array([phi, theta])
+
+
+def fx(state, angular_rates, dt):
+    phi = state[0]
+    theta = state[1]
 
     p = angular_rates.p
     q = angular_rates.q
@@ -19,7 +26,7 @@ def fx(estimated_state, angular_rates, dt):
     xdot[1] = q * math.cos(phi) - r * math.sin(phi)
     xdot[2] = q * math.sin(phi) / math.cos(theta) + r * math.cos(phi) / math.cos(theta)
 
-    return estimated_state + xdot * dt
+    return state + xdot * dt
 
 
 class EulerUKF:
@@ -47,10 +54,28 @@ class EulerUKF:
             prev_state, prev_error_covariance, self.kappa
         )
 
-        # Predict state and error covariance
+        # Predict state and associated error covariance
         f_sigma = np.zeros(shape=(self.n_state_vars, 2 * self.n_state_vars + 1))
         for col in range(2 * self.n_state_vars + 1):
             f_sigma[:, col] = fx(sigma[:, col], angular_rates_from_gyro, self.dt)
+        [predicted_state, predicted_error_covariance] = unscented_transformation(
+            f_sigma, weights, self.state_transition_noise_cov
+        )
+
+        # Predict measurement and associated error covariance
+        h_sigma = np.zeros(shape=(self.n_measurements, 2 * self.n_state_vars + 1))
+        for col in range(2 * self.n_state_vars + 1):
+            h_sigma[:, col] = hx(f_sigma[:, col])
+        [predicted_meas, predicted_meas_error_covariance] = unscented_transformation(
+            h_sigma, weights, self.measurement_noise_covariance
+        )
+
+        # Compute Kalman Gain
+        state_meas_covariance = np.zeros(shape=(self.n_state_vars, self.n_measurements))
+        for col in range(2 * self.n_state_vars + 1):
+            state_tmp = f_sigma[:, col].reshape(self.n_state_vars, 1) - predicted_state
+            meas_tmp = h_sigma[:, col].reshape(self.n_measurements, 1) - predicted_meas
+            state_meas_covariance += weights[col] * (state_tmp @ meas_tmp.transpose())
 
         return np.array(
             [
